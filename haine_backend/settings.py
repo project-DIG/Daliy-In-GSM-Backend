1165a7b5f5eee6b1d56abf193db6dde78042d71d
yohan <noyohanx@naver.com>
2022-11-08 16:20:26 +0900

    :wrench: setting.py 설정 추가

--- haine_backend/settings.py
+++ haine_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_restframework',
+    'haine_backend',
+    'haine_clip',
 ]
 
 MIDDLEWARE = [

3570d5ec938762db76d9c6b10b3ca9a128c9ad9b
yohan <noyohanx@naver.com>
2022-11-09 20:47:54 +0900

    :sparkles: User 모델 생성

--- haine_clip/models.py
+++ haine_clip/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    nickname = models.CharField('NICKNAME', max_length=20)
+    profile_image = models.URLField('PROFILE_IMAGE', max_length=255)
+    email = models.EmailField('EMAIL', max_length=255, unique=True)
+    bio = models.TextField('BIO')

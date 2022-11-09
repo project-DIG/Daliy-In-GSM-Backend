9f9c196ac814fed53fe7a2345f5f67ce0463b45f
yohan <noyohanx@naver.com>
2022-11-09 23:03:48 +0900

    :bug: user view 수정

--- haine_clip/views.py
+++ haine_clip/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
 
-class UserList(viewsets.ModelViewSet):
+class UserListViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
